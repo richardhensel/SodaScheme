7c3254b645df6d3f816757f86c06cff640c44d87
richardhensel <richardhensel20@gmail.com>
2019-06-17 06:02:34 +0000

    moved to api port

--- server/install.sh
+++ server/install.sh
@@ -1,10 +1,10 @@
-
+#! /bin/bash
 #apt-get install libapache2-mod-wsgi python-dev
 #apt-get install apache2
 #apt-get install python-pip
 #
-#pip install flask
-#pip install flask_restful
+#sudo pip install flask
+#sudo pip install flask_restful
 
 mkdir /var/www
 mkdir /var/www/Flask
@@ -15,5 +15,11 @@ mkdir /var/www/Flask/Scheme/static
 cp __init__.py /var/www/Flask/Scheme
 cp schemeapp.wsgi /var/www/Flask
 cp SchemeApp.conf /etc/apache2/sites-available
+cp SchemePorts.conf /etc/apache2/conf-available
 cp data/purchase_data.pkl /var/www/Flask/Scheme/data
+
+a2ensite SchemeApp.conf
+a2enconf SchemePorts.conf
+systemctl reload apache2
+systemctl restart apache2
 
