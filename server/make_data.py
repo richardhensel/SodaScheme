0d18c13bc6a488cc5f5ecc9d87b1a08723534064
Richard Hensel <richardhensel20@gmail.com>
2019-08-04 22:13:19 +0800

    fix

--- server/make_data.py
+++ server/make_data.py
@@ -1,5 +1,5 @@
 #data = {}
-#data["E.u2CiZrZ137BI.RTR8jsApZCHWYhVcTfhmJ9SOZFidDDMUb6DuRK8beKByrP7mmzXvIpvCvmy3YOFwD66Mny."] = {"Name": "R.Hensel", "Mobile": "0437109733", "Num_Purchases": 0}
-#data["t4gR6XhiGEwgCgYxn0HN5RCWUCIvYoKtCWxxH9AhIU8ndq0YehM7/P3oYHbK3dNJPJ3b9i2ig8YfTzUcqioxo1"] = {"Name": "D.Smith", "Mobile": "1234", "Num_Purchases": 0}
-#data["yTK/bUXq4Op638C8yvEaUQr4MYjSov7Sy52DUscptaN77yPNYdh4rE1.Gk7gRrs4Ow7ZXW.bkl56fki6tR9V21"] = {"Name": "N.Boswell", "Mobile": "1234", "Num_Purchases": 0}
-#data["rxin6MDrBt5MffsXhyLhtkI1EUXCajWIHXw6wqTt65VJrQy2nCniBlRkW37YXHXdadhM/PCSZ9defHBAkUNBg0"] = {"Name": "R.Mathew", "Mobile": "1234", "Num_Purchases": 0}
+#data["E.u2CiZrZ137BI.RTR8jsApZCHWYhVcTfhmJ9SOZFidDDMUb6DuRK8beKByrP7mmzXvIpvCvmy3YOFwD66Mny."] = {"Name": "hans", "Mobile": "1234", "Num_Purchases": 0}
+#data["t4gR6XhiGEwgCgYxn0HN5RCWUCIvYoKtCWxxH9AhIU8ndq0YehM7/P3oYHbK3dNJPJ3b9i2ig8YfTzUcqioxo1"] = {"Name": "smiff", "Mobile": "1234", "Num_Purchases": 0}
+#data["yTK/bUXq4Op638C8yvEaUQr4MYjSov7Sy52DUscptaN77yPNYdh4rE1.Gk7gRrs4Ow7ZXW.bkl56fki6tR9V21"] = {"Name": "wozzy", "Mobile": "1234", "Num_Purchases": 0}
+#data["rxin6MDrBt5MffsXhyLhtkI1EUXCajWIHXw6wqTt65VJrQy2nCniBlRkW37YXHXdadhM/PCSZ9defHBAkUNBg0"] = {"Name": "dontmess", "Mobile": "1234", "Num_Purchases": 0}
