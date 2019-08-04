fae4d62bf907b70bc4b7b12d2415adff280a450a
Richard Hensel <richardhensel20@gmail.com>
2019-08-04 14:54:58 +0800

    added to install script

--- reader/install.sh
+++ reader/install.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
+apt install i2c-tools
+apt install libnfc5 libnfc-bin libnfc-examples
+
+cp libnfc.conf /etc/nfc/
+
 cp nfc_reader.py /home/pi
 cp nfc_reader.service /etc/systemd/system/
